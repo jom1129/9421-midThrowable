0ccf911d4e647fce3540b273ffdbfd604257dffd
Kurt <kurtmtthw@gmail.com>
2021-10-18 19:16:12 +0800

    Second draft of the UI.

--- src/midlab1/Interface.java
+++ src/midlab1/Interface.java
@@ -3,12 +3,14 @@ package midlab1;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.net.PortUnreachableException;
 
 /*
     GUI CLASS: @Kurt
         You are free to contribute here, however.
  */
 public class Interface {
+    /*
     JPanel cards;   // panel that uses the CardLayout
     final static String infxtopfx = "Infix to Postfix";
     final static String evalpfx = "Evaluate Postfix";
@@ -60,5 +62,65 @@ public class Interface {
         });
     }
 
+
+     */
+    private static final int TEXTAREA_COLUMNS = 20;
+    private JPanel labelPanel = new JPanel();
+    private JPanel upperPanel = new JPanel();
+    private JTextField input = new JTextField(TEXTAREA_COLUMNS);
+    private JLabel expressionType = new JLabel("Postfix.");
+    private JPanel expressionPanel = new JPanel();
+    private JButton submit = new JButton("Submit");
+    private JComponent[] upperPanelComponents = { new JLabel("Expression: "), input };
+    private JComponent[] expressionPanelComponents = { new JLabel("Expression Type: "),
+            expressionType, submit};
+
+    public Interface() {
+        labelPanel.add(new JLabel("Infix-Postfix Utility"));
+        for (JComponent component : upperPanelComponents) upperPanel.add(component);
+        for (JComponent component : expressionPanelComponents) expressionPanel.add(component);
+        submit.addActionListener((ActionEvent e) -> expressionType.setText(input.getText()));
+    }
+
+    public JPanel getLabelPanel() {
+        return labelPanel;
+    }
+
+    public JPanel getUpperPanel() {
+        return upperPanel;
+    }
+
+    public JPanel getExpressionPanel() {
+        return expressionPanel;
+    }
+
+    private static void createAndShowGUI() {
+        Interface gui = new Interface();
+        JFrame frame = new JFrame();
+        // Use the Box Layout for the Content Pane
+        frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Add the panels
+        frame.getContentPane().add(gui.getLabelPanel());
+        frame.getContentPane().add(gui.getUpperPanel());
+        frame.getContentPane().add(gui.getExpressionPanel());
+
+        // Display the window
+        frame.pack();
+        frame.setTitle("Infix-Postfix Utility");
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                // Silence the compiler
+            }
+            createAndShowGUI();
+        });
+    }
 
 }
